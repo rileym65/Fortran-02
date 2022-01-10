5b9d0c5a53c5a5945ed1c2cf36a1451b9cec8a17
Mike Riley <mikehriley@hotmail.com>
2022-01-09 20:07:07 -0800

    Allow the use of ASCII and hex constants in DATA statements

--- cdata.c
+++ cdata.c
@@ -14,13 +14,14 @@ void cdata(char* line) {
   char token[32];
   int  vars[256];
   int  numVars;
-  int  i;
+  int  i,j;
   int  pos;
   int  v;
   int  var;
   int  count;
   int  isFloat;
   int  flag;
+  dword value;
   INTREAL ir;
   while (*line != 0) {
     numVars = 0;
@@ -62,7 +63,31 @@ void cdata(char* line) {
     isFloat = 0;
     flag = -1;
     while (flag) {
-      if (*line == '-') {
+      if (*line == '\'') {
+        line++;
+        pos = 0;
+        token[pos++] = '\'';
+        while (*line != '\'' && *line != 0) {
+          token[pos++] = *line++;
+          }
+        }
+      else if ((*line == 'x' || *line == 'X' || *line == 'z' || *line == 'Z') &&
+               *(line+1) == '\'') {
+        pos = 0;
+        token[pos++] = 'X';
+        line += 2;
+        while ((*line >= 'a' && *line <= 'f') ||
+               (*line >= 'A' && *line <= 'F') ||
+               (*line >= '0' && *line <= '9')) {
+          token[pos++] = *line++;
+          }
+        token[pos] = 0;
+        if (*line != '\'') {
+          showError("Syntax error");
+          return;
+          }
+        }
+      else if (*line == '-') {
         token[pos++] = *line;
         }
       else if (*line >= '0' && *line <= '9') {
@@ -83,9 +108,25 @@ void cdata(char* line) {
         }
       else if (*line == ',' || *line == '/' || *line == 0) {
         token[pos] = 0;
+        if (token[0] == '\'') {
+          ir.integer = 0;
+          for (j=1; j<strlen(token); j++)
+            ir.integer = (ir.integer << 8) | token[j];
+          }
+        else if (token[0] == 'X') {
+          ir.integer = 0;
+          for (j=1; j<strlen(token); j++) {
+            if (token[j] >= 'a' && token[j] <= 'f') 
+              ir.integer = (ir.integer << 4) + (token[j] - 87);
+            if (token[j] >= 'A' && token[j] <= 'F') 
+              ir.integer = (ir.integer << 4) + (token[j] - 55);
+            if (token[j] >= '0' && token[j] <= '9') 
+              ir.integer = (ir.integer << 4) + (token[j] - '0');
+            }
+          }
+        else if (isFloat) ir.real = atof(token);
+        else ir.integer = atoi(token);
         for (i=0; i<count; i++) {
-          if (isFloat) ir.real = atof(token);
-            else ir.integer = atoi(token);
           if (var >= numVars) {
             showError("Too many values specified");
             return;
@@ -93,6 +134,7 @@ void cdata(char* line) {
           variables[vars[var++]].value = ir.integer;
           }
         pos = 0;
+        count = 1;
         if (*line == '/' || *line == 0) {
           flag = 0;
           }
@@ -102,6 +144,10 @@ void cdata(char* line) {
         return;
         }
       if (flag) line++;
+
+
+
+
       }
     if (*line == '/') line++;
     }
