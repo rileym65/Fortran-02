9bbb28ddeea9eba9fcc3f7cd34c94f1d46c60b56
Mike Riley <mikehriley@hotmail.com>
2022-01-15 18:23:33 -0800

    Fixed bug where in some places REALs could be miscatergorized as INTEGERs

--- vartype.c
+++ vartype.c
@@ -15,7 +15,7 @@ char varType(int varNum) {
   if (variables[varNum].type == V_LOGICAL) return 'L';
   if (variables[varNum].type == V_SHORT) return 'S';
   if (variables[varNum].type == V_INTEGER) return 'I';
-  if (variables[varNum].type == V_INTEGER) return 'R';
+  if (variables[varNum].type == V_REAL) return 'R';
   if (variables[varNum].name[0] >= 'i' && variables[varNum].name[0] <= 'n') return 'I';
   if (variables[varNum].name[0] >= 'I' && variables[varNum].name[0] <= 'N') return 'I';
   return 'R';
